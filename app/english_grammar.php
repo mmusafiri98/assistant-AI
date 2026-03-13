e0adfc7abacd5b9d83fb88e1fca12d4560b91cb3
mmusafiri98 <118737447+mmusafiri98@users.noreply.github.com>
2026-03-13 18:12:15 +0100

    Create english_grammar.php

---
+++ app/english_grammar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>English Grammar Exercises</title>
+</head>
+
+<body>
+
+<h1>English Grammar Exercises</h1>
+
+<h3>Present Simple</h3>
+
+<p>Choose the correct sentence:</p>
+
+<form>
+
+<input type="radio" name="q1"> She go to school<br>
+<input type="radio" name="q1"> She goes to school<br>
+<input type="radio" name="q1"> She going to school<br>
+
+<br>
+
+<button type="submit">Check answer</button>
+
+</form>
+
+</body>
+</html>
