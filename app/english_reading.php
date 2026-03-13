2e0d45b3046cc0b8583657148a5895a54f0e9f80
mmusafiri98 <118737447+mmusafiri98@users.noreply.github.com>
2026-03-13 21:58:11 +0100

    Create english_reading.php

---
+++ app/english_reading.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reading Practice</title>
+</head>
+
+<body>
+
+<h1>Reading Practice</h1>
+
+<p>
+
+John is a student.  
+He lives in London.  
+Every morning he goes to school by bus.  
+
+</p>
+
+</body>
+</html>
